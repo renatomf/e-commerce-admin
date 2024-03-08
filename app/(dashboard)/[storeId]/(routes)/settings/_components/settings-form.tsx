5cac8e83b432c15c5c9f9772060b08dec214d582
renatomf <renatomar76@gmail.com>
2024-03-08 17:29:49 -0300

    Billboards Entity (Admin)

--- app/(dashboard)/[storeId]/(routes)/settings/_components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/_components/settings-form.tsx
@@ -24,6 +24,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { AlertModal } from "@/components/modals/alert-modal";
 import { ApiAlert } from "@/components/ui/api-alert";
+import { useOrigin } from "@/hooks/use-origin";
 
 interface SettingsFormProps {
   initialData: Store;
@@ -39,7 +40,8 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
   initialData
 }) => {
   const params = useParams();
-  const router = useRouter()
+  const router = useRouter();
+  const origin = useOrigin();
 
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
